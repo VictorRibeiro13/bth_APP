1f2462d0142928072e691d192eec33c538666ab7
VictorRibeiro13 <victorribeiro.docs.13@gmail.com>
2020-03-28 14:20:48 -0300

    Restructuring the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './Components/Header';
+
+// elemento HTML integrado em JS = JSX ( Javascript XML )
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Course: Omnistack
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    // É possível passar parametros para o component redenrizar
+    <Header title="Titulo do header"/>
+
   );
 }
 
